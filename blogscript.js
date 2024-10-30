b659c86539a1bf0bc961a2e6b57cb9d21a5a3bc8
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-30 16:00:44 +0100

    Add h1

--- blogscript.js
+++ blogscript.js
@@ -1,1 +1,19 @@
-console.log("HI");
+const express = require("express")
+
+const app = express()
+
+const port = 3003
+
+const host = "http://127.0.0.1:"
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) =>{
+    res.send("<h1>Benvenuto nel mio blog</h1>")
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server run on:${host}${port}`);
+    
+})
